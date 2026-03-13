64b0f0baebcb766474264c9252e0fdc838206343
Lakshaysharma077 <lakshaysharma866@gmail.com>
2026-03-13 13:00:02 +0530

    feat: integrate google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
 import { Analytics } from "@vercel/analytics/next"
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Class Track - Smart Attendance Tracker for Students & Teachers',
@@ -23,6 +24,12 @@ export default function RootLayout({
           <Toaster />
         </FirebaseClientProvider>
         <Analytics />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3555162840825683"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
